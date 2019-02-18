67e13f5327ef6c77c8ecf53172335ae59bcdb5fc
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-02-17 21:23:54 -0500

    Add help page

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('<h1>hello<h1>');
+    // res.send('<h1>hello<h1>');
+    res.send({
+        name: 'Yunier',
+        like: [
+            'Inline skate',
+            'code'
+        ]
+    });
 });
 
-app.listen(3000);
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
